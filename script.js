b300cd74db9439ef86771c28c88c2f90f3f968cb
mopedcow <188090362+mopedcow@users.noreply.github.com>
2025-04-08 15:38:20 +0100

    Create new function 'operate'

--- script.js
+++ script.js
@@ -4,9 +4,27 @@ function add(a, b) {
 function sub(a, b) {
     return a - b;
 }
-function multiply(a, b) {
+function mult(a, b) {
     return a * b;
 }
-function divide(a, b) {
+function div(a, b) {
     return a / b;
 }
+
+let num1;
+let num2;
+let operator;
+
+function operate(a, b, op) {
+    switch (op) {
+        case '+': return add(a, b);
+        case '-': return sub(a, b);
+        case '*': return mult(a, b);
+        case '/': return div(a,b);
+        default:
+            console.log('?');
+    }
+}
+
+console.log(operate(20, 4, '-'));
+
