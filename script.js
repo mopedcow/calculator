55c7468a80636756b5c93fe30624440a4f448cf1
mopedcow <188090362+mopedcow@users.noreply.github.com>
2025-04-09 14:51:52 +0100

    Make calculator work: now evaluates 2 numbers!! AC function also works now. Still can't work with single digits (lol). Still possible for a user-generated number to start with 0. Also need to work on calculating new equation from prev. result (eg. [2+2]+2)

--- script.js
+++ script.js
@@ -1,29 +1,49 @@
-function add(a, b) {
-    return a + b;
-}
-function sub(a, b) {
-    return a - b;
-}
-function mult(a, b) {
-    return a * b;
-}
-function div(a, b) {
-    return a / b;
-}
-
 let num1;
 let num2;
 let operator;
 //number currently active:
 let currNum = 0;
 
-let numDisplay = document.querySelector(".num-readout");
-let opDisplay = document.querySelector(".op-readout");
+const numDisplay = document.querySelector(".num-readout");
+const opDisplay = document.querySelector(".op-readout");
+const btns = document.querySelectorAll("button");
 
 numDisplay.textContent = currNum;
 opDisplay.textContent = operator;
 
-const btns = document.querySelectorAll("button");
+function add(a, b) { return a + b; }
+function sub(a, b) { return a - b; }
+function mult(a, b) { return a * b; }
+function div(a, b) { return a / b; }
+
+function operate(a, b, op) {
+    a = Number(a);
+    b = Number(b);
+    switch (op) {
+        case '+': return add(a, b);
+        case '-': return sub(a, b);
+        case 'x': return mult(a, b);
+        case '/': return div(a,b);
+        default:
+            console.log('?');
+}}
+
+function displayOperator(op) {
+    operator = op; // updates the variable
+    opDisplay.textContent = operator;
+}
+
+function displayNumber(num) {
+    numDisplay.textContent = num;
+}
+function clearAll() {
+    num1 = 0;
+    num2 = 0;
+    operator = "";
+    currNum = 0;
+    displayNumber(currNum);
+    displayOperator(operator);
+}
 
 btns.forEach((btn) => {
     btn.addEventListener("click", (e) => {
@@ -35,6 +55,16 @@ btns.forEach((btn) => {
                 operator = press;
                 currNum = 0;
                 displayOperator(operator);
+        } else if (press === "=") {
+            //if num1 & num2 present, evaluate expression & display result
+            if (num1 && num2) {
+                let result = operate(num1, num2, operator);
+                clearAll();
+                displayNumber(result);
+                displayOperator(operator);
+            }
+        } else if (press === "AC") {
+            clearAll();
         } else if ( Number(press) || press === "0") {
             //if currNum === 0, replace with 'press' (otherwise 0n)
             if (currNum === 0) {
@@ -60,12 +90,6 @@ btns.forEach((btn) => {
             console.log(`currNum: ${currNum} - num1: ${num1} - num2: ${num2}`);
             //if operator selected (!undefined), store values to num2 : num1
             
-        } else if (press === "=") {
-            //if num1 & num2 present, evaluate expression & display result
-            //clear operator, numX, currNum
-            operator = "";
-            displayOperator(operator);
-            console.log("= pressed");
         } else {
             console.log("working on it");
         }
@@ -93,25 +117,7 @@ btns.forEach((btn) => {
         } */
     })
 })
-function displayOperator(op) {
-    operator = op; // updates the variable
-    opDisplay.textContent = operator;
-}
 
-function displayNumber(num) {
-    numDisplay.textContent = num;
-}
-
-function operate(a, b, op) {
-    switch (op) {
-        case '+': return add(a, b);
-        case '-': return sub(a, b);
-        case '*': return mult(a, b);
-        case '/': return div(a,b);
-        default:
-            console.log('?');
-    }
-}
 /* an Extra Credit problem for later
 function allClear() {
     numReadout = 0;
